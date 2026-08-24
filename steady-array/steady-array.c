7bed96e6a079d5cd93563d3692e2989eb0b9ce0b
olhaux <oliver.l.haux@gmail.com>
2026-08-24 20:48:07 +0200

    Add max intervall (replaces maxAll)

--- steady-array/steady-array.c
+++ steady-array/steady-array.c
@@ -1,15 +1,13 @@
-
-
 #include <stdio.h>
 
-#define MAX_NODES 50000000
+#define MAX_NODES 20000000
 
 #define MAX_VERSIONS 1000001
 
 struct Node{
     int max;
     int child[2];
-};  
+};
 
 struct Node pool[MAX_NODES];
 int nodeCount = 1;
@@ -49,6 +47,28 @@ int bigger(int a, int b){
     return b;
 }
 
+int minBits(unsigned int index){
+    int bits = 0;
+    while(index > 0){
+        bits++;
+        index = index >> 1;
+    }
+    return bits;
+}
+
+int grow(int root, int level, int target){
+    while(level < target){
+        if(root != 0){
+            int n = newNode();
+            pool[n].child[0] = root;
+            pool[n].max = pool[root].max;
+            root = n;
+        }
+        level++;
+    }
+    return root;
+}
+
 int set(int old, int level, unsigned int index, int val){
     int n = newNode();
     int bitN;
@@ -59,7 +79,7 @@ int set(int old, int level, unsigned int index, int val){
         return n;
     }
 
-    bitN = bit(index, level -1);
+    bitN = bit(index, level - 1);
     oldChild = 0;
     if(old != 0){
         oldChild = pool[old].child[bitN];
@@ -75,16 +95,78 @@ int set(int old, int level, unsigned int index, int val){
     return n;
 }
 
-int get(int node, int level, unsigned index){
-        while (node != 0 && level > 0) {
+int get(int node, int level, unsigned int index){
+    while(node != 0 && level > 0){
         node = pool[node].child[bit(index, level - 1)];
         level = level - 1;
     }
-    if (node == 0) {
+    if(node == 0){
         return 0;
     }
     return pool[node].max;
 }
+
+int maxRight(int node, int level, unsigned int left){
+    if(node == 0){
+        return -1;
+    }
+    if(level == 0){
+        return pool[node].max;
+    }
+    if(bit(left, level - 1) == 1){
+        return maxRight(pool[node].child[1], level - 1, left);
+    }
+    return bigger(maxRight(pool[node].child[0], level - 1, left), maxSub(pool[node].child[1]));
+}
+
+int maxLeft(int node, int level, unsigned int right){
+    if(node == 0){
+        return -1;
+    }
+    if(level == 0){
+        return pool[node].max;
+    }
+    if(bit(right, level - 1) == 0){
+        return maxLeft(pool[node].child[0], level - 1, right);
+    }
+    return bigger(maxSub(pool[node].child[0]), maxLeft(pool[node].child[1], level - 1, right));
+}
+
+int maxSegment(int node, int level, unsigned int left, unsigned int right){
+    int leftBit, rightBit;
+
+    if(node == 0){
+        return -1;
+    }
+    if(level == 0){
+        return pool[node].max;
+    }
+
+    leftBit = bit(left, level - 1);
+    rightBit = bit(right, level - 1);
+
+    if(leftBit == 0 && rightBit == 0){
+        return maxSegment(pool[node].child[0], level - 1, left, right);
+    }
+    if(leftBit == 1 && rightBit == 1){
+        return maxSegment(pool[node].child[1], level - 1, left, right);
+    }
+    return bigger(maxRight(pool[node].child[0], level - 1, left), maxLeft(pool[node].child[1], level - 1, right));
+}
+
+int maxInInterval(int root, int level, unsigned int left, unsigned int right){
+    if(!fits(left, level)){
+        return 0;
+    }
+    if(!fits(right, level)){
+        right = (1u << level) - 1;
+    }
+    if(left > right){
+        return 0;
+    }
+    return bigger(maxSegment(root, level, left, right), 0);
+}
+
 int main(){
     char input[20];
     roots[0] = 0;
@@ -92,49 +174,55 @@ int main(){
 
     while(scanf("%19s", input) == 1){
 
-        int root = roots[version -1];
-        int level = levels[version-1];
+        int root = roots[version - 1];
+        int level = levels[version - 1];
 
-        if(input[0] == 's'){
-            unsigned int index, temp;
-            int val, minBits;
+        switch(input[0]){
 
-            scanf("%u %d", &index, &val);
-
-            minBits = 0;
-            temp = index;
-            while (temp > 0){
-                minBits++;
-                temp = temp >> 1;
-            }
-
-            while (level < minBits){
-                if(root != 0){
-                    int n = newNode();
-                    pool[n].child[0] = root;
-                    pool[n].max = pool[root].max;
-                    root = n;
-                }
-                level++;
-            }
-            roots[version] = set(root, level, index, val);
-            levels[version] = level;
-            version++;
-        }else if(input[0] == 'g'){
+            case 's': {
                 unsigned int index;
+                int val;
+
+                scanf("%u %d", &index, &val);
+
+                level = bigger(level, minBits(index));
+                root = grow(root, levels[version - 1], level);
+
+                roots[version] = set(root, level, index, val);
+                levels[version] = level;
+                version++;
+                break;
+            }
+
+            case 'g': {
+                unsigned int index;
+
                 scanf("%u", &index);
+
                 if(fits(index, level)){
                     printf("%d\n", get(root, level, index));
                 } else {
                     printf("0\n");
                 }
-            } else if(input[0] == 'u'){
-            if(version > 1){
-                version--;
+                break;
             }
-        } else if(input[0] == 'm'){
-            printf("%d\n", bigger(maxSub(root), 0));
+
+            case 'm': {
+                unsigned int left, right;
+
+                scanf("%u %u", &left, &right);
+
+                printf("%d\n", maxInInterval(root, level, left, right));
+                break;
+            }
+
+            case 'u': {
+                if(version > 1){
+                    version--;
+                }
+                break;
+            }
         }
-    }     
-    return 0; 
+    }
+    return 0;
 }
